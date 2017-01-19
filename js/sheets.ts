681bdbf2aa6eaa126fcf6941a0b533d21074295c
AMDraisma <Lumwymad,nbr>
2017-01-19 15:45:50 +0100

    Added init and loadsprites function

--- js/sheets.ts
+++ js/sheets.ts
@@ -1,2 +1,24 @@
 /// <reference path="../typings.d.ts" />
 
+
+function init() {
+    var div = document.getElementById('scenediv');
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(div.clientWidth, div.clientHeight);
+    div.appendChild(renderer.domElement);
+}
+
+function loadSprites() {
+    var promise = new Promise((resolve, reject) => {
+        var sprites = $
+        sprites.forEach((sprite)=>{
+
+        }).then(() => {
+            resolve();
+        })
+    })
+    return promise;
+}
