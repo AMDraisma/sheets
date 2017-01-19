1331f2732c443a4348e126ff1c52c71bf6103b1e
AMDraisma <Lumwymad,nbr>
2017-01-19 20:54:16 +0100

    reworked sprites (wip)

--- js/sheets.ts
+++ js/sheets.ts
@@ -1,24 +1,38 @@
-/// <reference path="../typings.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 
+class spriteJson {
+    name: string;
+    imgurl: string;
+}
 
 function init() {
     var div = document.getElementById('scenediv');
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var camera = new THREE.OrthographicCamera(div.clientWidth/2, div.clientWidth/2, div.clientHeight/2, div.clientHeight/2, 1, 1000);
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(div.clientWidth, div.clientHeight);
     div.appendChild(renderer.domElement);
 }
 
+var sprites: {[name: string]: Object} = { };
+
 function loadSprites() {
     var promise = new Promise((resolve, reject) => {
-        var sprites = $
-        sprites.forEach((sprite)=>{
+        $.get('js/sprites.json')
+        .then((spriteJson) => {
+            var spriteMap;
+            var spriteMat;
+            spriteJson.forEach((sprite: spriteJson)=>{
+                spriteMap = new THREE.TextureLoader().load(sprite.imgurl);
+                spriteMat = new THREE.SpriteMaterial();
 
-        }).then(() => {
-            resolve();
-        })
-    })
+                sprites[sprite.name] = new THREE.Sprite();
+            })
+            .then(() => {
+                resolve();
+            });
+        });
+    });
     return promise;
 }
